9e83176bfe6104aef08dea175329079b64ec0f6e
Raneem-Khafagy <raneemkhfagy@gmail.com>
2021-02-09 05:21:51 +0200

    .

--- lib/Views/Screens/NotesHomePage.dart
+++ lib/Views/Screens/NotesHomePage.dart
@@ -14,7 +14,7 @@ class NotesHomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     double width = MediaQuery.of(context).size.width;
     double height = MediaQuery.of(context).size.height;
-    // Provider.of<NoteProvider>(context, listen: false).notesWidgetsList();
+    Provider.of<NoteProvider>(context, listen: false).notesWidgetsList();
     List<Widget> cards =
         Provider.of<NoteProvider>(context, listen: false).noteCards;
     return Scaffold(
@@ -36,81 +36,6 @@ class NotesHomePage extends StatelessWidget {
             ],
           ),
           CircularTodayDate(),
-
-          // Positioned(
-          //   top: height * .16,
-          //   height: height * 0.76,
-          //   width: width,
-          //   child: Container(
-          //     height: height * 0.7,
-          //     width: width,
-          //     child: ListView(
-          //       children: [
-          //         NoteWidget(
-          //           noteDescription:
-          //               "ddddddd dssss sssss  ssss ssss ssssss ssssss ssssss sssssss ssssss ssssss ssssss dddddd",
-          //           noteTitle: "xxxxxxxxxxxx",
-          //           noteDate: 'ascasc',
-          //         ),
-          //         NoteWidget(
-          //           noteDescription:
-          //               "ddddddd dssss sssss  ssss ssss ssssss ssssss ssssss sssssss ssssss ssssss ssssss dddddd",
-          //           noteTitle: "xxxxxxxxxxxx",
-          //           noteDate: 'ascasc',
-          //         ),
-          //         NoteWidget(
-          //           noteDescription:
-          //               "ddddddd dssss sssss  ssss ssss ssssss ssssss ssssss sssssss ssssss ssssss ssssss dddddd",
-          //           noteTitle: "xxxxxxxxxxxx",
-          //           noteDate: 'ascasc',
-          //         ),
-          //         NoteWidget(
-          //           noteDescription:
-          //               "ddddddd dssss sssss  ssss ssss ssssss ssssss ssssss sssssss ssssss ssssss ssssss dddddd",
-          //           noteTitle: "xxxxxxxxxxxx",
-          //           noteDate: 'ascasc',
-          //         ),
-          //         NoteWidget(
-          //           noteDescription:
-          //               "ddddddd dssss sssss  ssss ssss ssssss ssssss ssssss sssssss ssssss ssssss ssssss dddddd",
-          //           noteTitle: "xxxxxxxxxxxx",
-          //           noteDate: 'ascasc',
-          //         ),
-          //         NoteWidget(
-          //           noteDescription:
-          //               "ddddddd dssss sssss  ssss ssss ssssss ssssss ssssss sssssss ssssss ssssss ssssss dddddd",
-          //           noteTitle: "xxxxxxxxxxxx",
-          //           noteDate: 'ascasc',
-          //         ),
-          //         NoteWidget(
-          //           noteDescription:
-          //               "ddddddd dssss sssss  ssss ssss ssssss ssssss ssssss sssssss ssssss ssssss ssssss dddddd",
-          //           noteTitle: "xxxxxxxxxxxx",
-          //           noteDate: 'ascasc',
-          //         ),
-          //         NoteWidget(
-          //           noteDescription:
-          //               "ddddddd dssss sssss  ssss ssss ssssss ssssss ssssss sssssss ssssss ssssss ssssss dddddd",
-          //           noteTitle: "xxxxxxxxxxxx",
-          //           noteDate: 'ascasc',
-          //         ),
-          //         NoteWidget(
-          //           noteDescription:
-          //               "ddddddd dssss sssss  ssss ssss ssssss ssssss ssssss sssssss ssssss ssssss ssssss dddddd",
-          //           noteTitle: "xxxxxxxxxxxx",
-          //           noteDate: 'ascasc',
-          //         ),
-          //         NoteWidget(
-          //           noteDescription:
-          //               "ddddddd dssss ddmd ddd ddd ddd ddd ddd ddd ddd ddd dd dd ddddd dddd dddd dddd ",
-          //           noteTitle: "xxxxxxxxxxxx",
-          //           noteDate: 'ascasc',
-          //         ),
-          //       ],
-          //     ),
-          //   ),
-          // ),
-
           Positioned(
             top: height * .16,
             height: height * 0.76,
@@ -127,3 +52,77 @@ class NotesHomePage extends StatelessWidget {
     );
   }
 }
+
+// Positioned(
+//   top: height * .16,
+//   height: height * 0.76,
+//   width: width,
+//   child: Container(
+//     height: height * 0.7,
+//     width: width,
+//     child: ListView(
+//       children: [
+//         NoteWidget(
+//           noteDescription:
+//               "ddddddd dssss sssss  ssss ssss ssssss ssssss ssssss sssssss ssssss ssssss ssssss dddddd",
+//           noteTitle: "xxxxxxxxxxxx",
+//           noteDate: 'ascasc',
+//         ),
+//         NoteWidget(
+//           noteDescription:
+//               "ddddddd dssss sssss  ssss ssss ssssss ssssss ssssss sssssss ssssss ssssss ssssss dddddd",
+//           noteTitle: "xxxxxxxxxxxx",
+//           noteDate: 'ascasc',
+//         ),
+//         NoteWidget(
+//           noteDescription:
+//               "ddddddd dssss sssss  ssss ssss ssssss ssssss ssssss sssssss ssssss ssssss ssssss dddddd",
+//           noteTitle: "xxxxxxxxxxxx",
+//           noteDate: 'ascasc',
+//         ),
+//         NoteWidget(
+//           noteDescription:
+//               "ddddddd dssss sssss  ssss ssss ssssss ssssss ssssss sssssss ssssss ssssss ssssss dddddd",
+//           noteTitle: "xxxxxxxxxxxx",
+//           noteDate: 'ascasc',
+//         ),
+//         NoteWidget(
+//           noteDescription:
+//               "ddddddd dssss sssss  ssss ssss ssssss ssssss ssssss sssssss ssssss ssssss ssssss dddddd",
+//           noteTitle: "xxxxxxxxxxxx",
+//           noteDate: 'ascasc',
+//         ),
+//         NoteWidget(
+//           noteDescription:
+//               "ddddddd dssss sssss  ssss ssss ssssss ssssss ssssss sssssss ssssss ssssss ssssss dddddd",
+//           noteTitle: "xxxxxxxxxxxx",
+//           noteDate: 'ascasc',
+//         ),
+//         NoteWidget(
+//           noteDescription:
+//               "ddddddd dssss sssss  ssss ssss ssssss ssssss ssssss sssssss ssssss ssssss ssssss dddddd",
+//           noteTitle: "xxxxxxxxxxxx",
+//           noteDate: 'ascasc',
+//         ),
+//         NoteWidget(
+//           noteDescription:
+//               "ddddddd dssss sssss  ssss ssss ssssss ssssss ssssss sssssss ssssss ssssss ssssss dddddd",
+//           noteTitle: "xxxxxxxxxxxx",
+//           noteDate: 'ascasc',
+//         ),
+//         NoteWidget(
+//           noteDescription:
+//               "ddddddd dssss sssss  ssss ssss ssssss ssssss ssssss sssssss ssssss ssssss ssssss dddddd",
+//           noteTitle: "xxxxxxxxxxxx",
+//           noteDate: 'ascasc',
+//         ),
+//         NoteWidget(
+//           noteDescription:
+//               "ddddddd dssss ddmd ddd ddd ddd ddd ddd ddd ddd ddd dd dd ddddd dddd dddd dddd ",
+//           noteTitle: "xxxxxxxxxxxx",
+//           noteDate: 'ascasc',
+//         ),
+//       ],
+//     ),
+//   ),
+// ),
